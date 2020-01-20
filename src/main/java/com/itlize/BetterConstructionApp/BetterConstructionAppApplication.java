ba3d98212babb1c314a5c42158807669226e62b0
BogacSabuncu <bsabuncu@mac.com>
2020-01-20 16:22:04 -0500

    initial files

---
+++ src/main/java/com/itlize/BetterConstructionApp/BetterConstructionAppApplication.java
@@ -0,0 +1,13 @@
+package com.itlize.BetterConstructionApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterConstructionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterConstructionAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itlize/BetterConstructionApp/BetterConstructionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itlize.BetterConstructionApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterConstructionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
